9afe620d461c8f65cc989f266197c507523d15dc
mazeikaite <morta.mazeikaite@student.howest.be>
2025-01-22 09:37:39 +0100

    responsive images added

--- src/main.js
+++ src/main.js
@@ -1,24 +1,13 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const menuToggle = document.getElementById('menu-toggle');
+const nav = document.getElementById('navigation');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+document.addEventListener("DOMContentLoaded", function () {
+    if (menuToggle && nav) {
+        menuToggle.addEventListener("click", function () {
+            menuToggle.classList.toggle("open");
+            nav.classList.toggle("open");
+        });
+        } else {
+            console.error("Hamburger or menu element not found in the DOM.");
+        }
+});
